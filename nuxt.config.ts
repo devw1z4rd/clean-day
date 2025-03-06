6f6e3fcd017a568a9bc0830f98bf4c89d3cccf12
devw1z4rd <richard00111010@gmail.com>
2025-03-06 01:29:31 +0100

    lol

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,9 @@
 export default defineNuxtConfig({
-  ssr: false,
-  
-  app: {
-    baseURL: '/clean-day/',
-    buildAssetsDir: '/_nuxt/'
-  },
-  
-  nitro: {
-    preset: 'github-pages'
-  },
-  
-  devtools: { enabled: true },
-  
-  typescript: {
-    strict: true
-  },
-  
-  experimental: {
-    renderJsonPayloads: true
-  },
-  
   modules: [
     '@nuxt/ui',
     '@pinia/nuxt',
     '@nuxtjs/color-mode',
     '@nuxtjs/tailwindcss',
   ],
+  compatibilityDate: '2025-03-06',
 })
